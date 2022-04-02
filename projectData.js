d0d380e1ce2afbf58cc5095d219b3cda7dde07d0
takacsnorbee <38784102+takacsnorbee@users.noreply.github.com>
2022-04-02 09:10:50 +0200

    outsourced projectsdata, tricky button added

---
+++ projectData.js
@@ -0,0 +1,216 @@
+/* PROJECTSDATA should be fetched from a server */
+
+const PROJECTSDATA = [
+    {
+        sectionName: 'chrome-box',
+        elementName: 'blur-all',
+        demoURL: 'https://github.com/takacsnorbee/chrome_extension_blur',
+        imgSrc: 'pic/blur.png',
+        figcaptionText: 'Blur Img'
+    },
+    {
+        sectionName: 'vue-box',
+        elementName: 'word-cards',
+        demoURL: 'https://github.com/takacsnorbee/wordCards',
+        imgSrc: 'pic/wordCard.png',
+        figcaptionText: 'Words'
+    },
+    {
+        sectionName: 'vue-box',
+        elementName: 'dog-fight',
+        demoURL: 'https://github.com/takacsnorbee/dogFight',
+        imgSrc: 'pic/dogFight.png',
+        figcaptionText: 'Dog Fight'
+    },
+    {
+        sectionName: 'webler-box',
+        elementName: 'shopping-list-java',
+        demoURL: 'https://github.com/takacsnorbee/Shopping_List_Java',
+        imgSrc: 'pic/shopping-list.png',
+        figcaptionText: 'Shopping List (java)'
+    },
+    {
+        sectionName: 'webler-box',
+        elementName: 'game-center',
+        demoURL: 'https://github.com/takacsnorbee/game_center_java',
+        imgSrc: 'pic/game-center.png',
+        figcaptionText: 'Game center(Java)'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'profil-cb',
+        demoURL: 'https://github.com/takacsnorbee/profile',
+        imgSrc: 'pic/portfolio-cb.png',
+        figcaptionText: 'Profile'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'color-clock',
+        demoURL: 'https://github.com/takacsnorbee/colorclock',
+        imgSrc: 'pic/colorclock.png',
+        figcaptionText: 'Color Clock'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'mantis',
+        demoURL: 'https://github.com/takacsnorbee/Mantis-shrimps',
+        imgSrc: 'pic/mantis.png',
+        figcaptionText: 'Mantis'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'c-light',
+        demoURL: 'https://github.com/takacsnorbee/codeberry-light/',
+        imgSrc: 'pic/light.png',
+        figcaptionText: 'Blog'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'wedding',
+        demoURL: 'https://github.com/takacsnorbee/wedding',
+        imgSrc: 'pic/wedding.png',
+        figcaptionText: 'Wedding'
+    },
+    {
+        sectionName: 'cb-box',
+        elementName: 'wiki',
+        demoURL: 'https://github.com/takacsnorbee/wiki-page-copy',
+        imgSrc: 'pic/wiki.png',
+        figcaptionText: 'Wiki Copy'
+    },
+    {
+        sectionName: 'fcc-box',
+        elementName: 'tribute',
+        demoURL: 'https://github.com/takacsnorbee/tribute-page-fcc',
+        imgSrc: 'pic/tribute.png',
+        figcaptionText: 'Triubute'
+    },
+    {
+        sectionName: 'fcc-box',
+        elementName: 'survey',
+        demoURL: 'https://github.com/takacsnorbee/survey-form-fcc/',
+        imgSrc: 'pic/survey.png',
+        figcaptionText: 'Survey'
+    },
+    {
+        sectionName: 'fcc-box',
+        elementName: 'product',
+        demoURL: 'https://github.com/takacsnorbee/product-landing-page-fcc/',
+        imgSrc: 'pic/product-landing.png',
+        figcaptionText: 'Landing Page'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'shooter',
+        demoURL: 'https://github.com/takacsnorbee/smileShooter/',
+        imgSrc: 'pic/shooter.png',
+        figcaptionText: 'Shooter'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'udemy',
+        demoURL: 'https://github.com/takacsnorbee/udemy/',
+        imgSrc: 'pic/udemy.png',
+        figcaptionText: 'Udemy'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'shopping-list',
+        demoURL: 'https://github.com/takacsnorbee/Shopping-list/',
+        imgSrc: 'pic/shopping-list.png',
+        figcaptionText: 'Shopping List'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'd-clock',
+        demoURL: 'https://github.com/takacsnorbee/digitalClock/',
+        imgSrc: 'pic/digital.png',
+        figcaptionText: 'Digital Clock'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'a-clock',
+        demoURL: 'https://github.com/takacsnorbee/analogClock/',
+        imgSrc: 'pic/analoge.png',
+        figcaptionText: 'Analoge Clock'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'game',
+        demoURL: 'https://github.com/takacsnorbee/ticTacToe/',
+        imgSrc: 'pic/tic-tac-toe.png',
+        figcaptionText: 'Tic Tac Toe'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'spinner',
+        demoURL: 'https://github.com/takacsnorbee/loader-spinner/',
+        imgSrc: 'pic/spinner.png',
+        figcaptionText: 'Spinner'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'progress-bar',
+        demoURL: 'https://github.com/takacsnorbee/progress_bar/',
+        imgSrc: 'pic/progress-bar.png',
+        figcaptionText: 'Progress Bar'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'search-light',
+        demoURL: 'https://github.com/takacsnorbee/searchlight/',
+        imgSrc: 'pic/searchlight.png',
+        figcaptionText: 'Search Light'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'menu-indicator',
+        demoURL: 'https://github.com/takacsnorbee/menu-indicator/',
+        imgSrc: 'pic/menu-indicator.png',
+        figcaptionText: 'Menu With Indicator'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'fingerprint-scanner',
+        demoURL: 'https://github.com/takacsnorbee/fingerprint/',
+        imgSrc: 'pic/fingerprint-scanner.png',
+        figcaptionText: 'Fingerprint Scanner'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'click-signal',
+        demoURL: 'https://github.com/takacsnorbee/click_signal/',
+        imgSrc: 'pic/click_signal.png',
+        figcaptionText: 'Click Signal'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'snake-border',
+        demoURL: 'https://github.com/takacsnorbee/snake_border/',
+        imgSrc: 'pic/snake_border.png',
+        figcaptionText: 'Snake Border'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'toggle-button-v1',
+        demoURL: 'https://github.com/takacsnorbee/toggle_button_v1/',
+        imgSrc: 'pic/toggle_button_v1.png',
+        figcaptionText: 'Toggle Button V1'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'toggle-button-v2',
+        demoURL: 'https://github.com/takacsnorbee/toggle_button_v1/',
+        imgSrc: 'pic/toggle_button_v2.png',
+        figcaptionText: 'Toggle Button V2'
+    },
+    {
+        sectionName: 'js-box',
+        elementName: 'tricky-btn',
+        demoURL: 'https://github.com/takacsnorbee/tricky_btn/',
+        imgSrc: 'pic/tricky_btn.png',
+        figcaptionText: 'Tricky Btn'
+    },
+];
+
+export default PROJECTSDATA;
